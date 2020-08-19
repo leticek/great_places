cbb104a69de206693368823d562b48d1a1f9e0bf
leticek <smiead@seznam.cz>
2020-08-19 22:42:43 +0200

    SQLite db connected, you can now store on device

--- lib/providers/place_provider.dart
+++ lib/providers/place_provider.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:great_places/helpers/db_helper.dart';
 import 'dart:io';
 
 import '../models/place.dart';
+import '../helpers/db_helper.dart';
 
 class Places with ChangeNotifier {
   List<Place> _items = [];
@@ -15,6 +17,25 @@ class Places with ChangeNotifier {
         title: title,
         location: null);
     _items.add(newPlace);
+    notifyListeners();
+    DBHelper.insert('user_places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+      //'location': newPlace.location
+    });
+  }
+
+  Future<void> selectAndSet() async {
+    final dataList = await DBHelper.select('user_places');
+    _items = dataList
+        .map((map) => Place(
+              id: map['id'],
+              image: File(map['image']),
+              location: null,
+              title: map['title'],
+            ))
+        .toList();
     notifyListeners();
   }
 }
--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -19,22 +19,30 @@ class PlacesListScreen extends StatelessWidget {
           )
         ],
       ),
-      body: Consumer<Places>(
-        builder: (context, places, child) => places.items.length <= 0
-            ? AddYourFirstPlace()
-            : ListView.builder(
-                itemCount: places.items.length,
-                itemBuilder: (context, index) => ListTile(
-                  leading: CircleAvatar(
-                    backgroundImage: FileImage(
-                      places.items.elementAt(index).image,
-                    ),
-                  ),
-                  title: Text(places.items[index].title),
-                  onTap: () {
-                    // Go to detail page ...
-                  },
-                ),
+      body: FutureBuilder(
+        future: Provider.of<Places>(context, listen: false).selectAndSet(),
+        builder: (context, snapshot) => snapshot.connectionState ==
+                ConnectionState.waiting
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : Consumer<Places>(
+                builder: (context, places, child) => places.items.length <= 0
+                    ? AddYourFirstPlace()
+                    : ListView.builder(
+                        itemCount: places.items.length,
+                        itemBuilder: (context, index) => ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage: FileImage(
+                              places.items.elementAt(index).image,
+                            ),
+                          ),
+                          title: Text(places.items[index].title),
+                          onTap: () {
+                            // Go to detail page ...
+                          },
+                        ),
+                      ),
               ),
       ),
     );
